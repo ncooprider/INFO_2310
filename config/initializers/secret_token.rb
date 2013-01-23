fc310d5f292be81b36eb79b873222aed7cccd7e8
ncooprider <nmc58@cornell.edu>
2013-01-23 19:26:10 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Info2310::Application.config.secret_token = '67622e7fa300b4b8e6bf8cafd783b83e76e9c293c54c79409852589c6ac30708dbfe81f33fc7ec78a7c4987200481c62fa6a341b6caeab14a09ae5f6ab676b89'

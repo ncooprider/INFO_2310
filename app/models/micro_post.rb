a38ebf71de8dca5f9fcc9cc6603cc3ce574541b8
ncooprider <nmc58@cornell.edu>
2013-02-13 19:43:36 +0000

    User Model

--- app/models/micro_post.rb
+++ app/models/micro_post.rb
@@ -1,6 +1,8 @@
 class MicroPost < ActiveRecord::Base
   attr_accessible :user_id, :content
   
+  belongs_to :user
+  
   validates :content, presence: true,
                       length: { minimum: 5, maximum: 140 }
   validates :user_id, presence: true

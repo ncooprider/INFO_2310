fa9885fc8b820878433e1c5c2464df50177fdbee
ncooprider <nmc58@cornell.edu>
2013-02-20 19:48:26 +0000

    authentication with 2 failing specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Info2310::Application.routes.draw do
   resources :micro_posts
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   get "static_pages/home"
   get "static_pages/help"
